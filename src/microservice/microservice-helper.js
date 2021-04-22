d8d83f04ec193978f61b21d5026417f5e31f8bd5
Kilian <kilian@visuals3d.de>
2021-04-22 23:51:07 +0200

    feature: added readme and auto api interface generation from open-api file

--- src/microservice/microservice-helper.js
+++ src/microservice/microservice-helper.js
@@ -2,11 +2,11 @@ import fs from 'fs';
 import path from 'path';
 import { replaceStringInFile } from '../helpers';
 
-
+const replaceableVarsFilesList = ['package.json', 'swagger.yaml', 'preset.env', 'docker-compose.yml', 'README.md'];
 
 export async function initMicroservice(targetPath) {
-    const serviceName = path.basename(targetPath);;
-    await replaceStringInFile(path.join(targetPath, 'package.json'), 'MICROSERVICE_NAME', serviceName);
-    await replaceStringInFile(path.join(targetPath, 'swagger.yaml'), 'MICROSERVICE_NAME', serviceName);
-    await replaceStringInFile(path.join(targetPath, 'preset.env'), 'MICROSERVICE_NAME', serviceName);
+    const serviceName = path.basename(targetPath);
+    for (let file of replaceableVarsFilesList) {
+        await replaceStringInFile(path.join(targetPath, file), 'MICROSERVICE_NAME', serviceName);
+    }
 }
